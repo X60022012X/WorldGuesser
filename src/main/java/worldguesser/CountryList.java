2ea6fbd0a71dbe9807068e2d04b172db9ace072c
X60022012X <144954718+X60022012X@users.noreply.github.com>
2026-03-23 16:54:51 +0100

    1

---
+++ src/main/java/worldguesser/CountryList.java
@@ -0,0 +1,20 @@
+package worldguesser;
+
+import java.util.Map;
+
+public class CountryList {
+
+    public static final Map<String, String> countryList = Map.of(
+        "tr", "Turkey",
+        "sweden", "Sverige",
+        "denmark", "Danmark",
+        "finland", "Finland",
+        "germany", "Tyskland",
+        "france", "Frankrike",
+        "spain", "Spania",
+        "italy", "Italia",
+        "poland", "Polen",
+        "netherlands", "Nederland"
+
+    );
+}
