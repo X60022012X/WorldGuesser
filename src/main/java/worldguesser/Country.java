0bc6bab7890c904495cf98c49ae0b108b32afc66
stormurr <stormur.romundset@gmail.com>
2026-03-23 16:32:34 +0100

    Country class

--- src/main/java/worldguesser/Country.java
+++ src/main/java/worldguesser/Country.java
@@ -1,5 +1,28 @@
 package worldguesser;
 
 public class Country {
-    
+
+    private String name;
+    private SVGPath shape;
+
+    public Country(String name, SVGPath shape) {
+        this.name = name;
+        this.shape = shape;
+    }
+
+    public void setCorrect() {
+        shape.setFill(Color.GREEN);
+    }
+
+    public void setWrong() {
+        shape.setFill(Color.RED);
+    }
+
+    public void reset() {
+        shape.setFill(Color.GRAY);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
